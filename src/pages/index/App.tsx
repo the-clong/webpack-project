2320f6a65c9e59f1e4172a129a07f38dcbb77841
chenglong08 <chenglong08@58.com>
2021-12-23 16:18:54 +0800

    fix:eslint

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import './index.css';
+
 function App() {
     const [count, setCount] = useState(0);
 
     return (
- <div>
-    {count}
-    <button type="button" onClick={() => setCount(count + 1)}>
-        1
-        </button></div>
-    )
-};
+        <div>
+            {count}
+            <button type="button" onClick={() => setCount(count + 1)}>
+                1
+            </button>
+
+        </div>
+    );
+}
+
 export default App;
